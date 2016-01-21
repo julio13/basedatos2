9da96d495ca6b5c554d1e77a101fb61605f6320f
julio13 <juuliuus@outlook.com>
2016-01-20 21:32:42 -0600

    Update practica1.sql

--- practica1.sql
+++ practica1.sql
@@ -1,1 +1,18 @@
+--Crearemos una tabla muy simple para probar el poder de PL sql
 
+create table DANCE(id_dance integer, nombre varchar2(40), edad integer);
+--prendemos el servidor en modo de desarrollador
+set serveroutput on;
+--vamos a empezar con un bloque PL SQL 
+DECLARE 
+--AQUI SE DECLARA LAS VAROABLES A USARSE
+edad integer;
+YEARS INTEGER;
+BEGIN
+--AQUI VA LA LOGICA DEL PROGRAMA
+YEARS:=21;
+edad:=YEARS*365;
+
+DBMS_OUTPUT.PUT_LINE('TU EDAD EN DIA ES'|| edad);
+END;
+/
