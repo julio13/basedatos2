ef0ed125239d0ecfa308f34cc0b83f318ae0fa1f
julio13 <juuliuus@outlook.com>
2016-01-30 10:53:10 -0600

    Update practica1.sql

--- practica1.sql
+++ practica1.sql
@@ -1,40 +1,83 @@
-create table DANCE(id_dance integer, nombre varchar2(40), edad integer);
---prendemos el servidor en modo de desarrollador
-set serveroutput on;
---vamos a empezar con un bloque PL SQL 
-DECLARE 
---AQUI SE DECLARA LAS VAROABLES A USARSE
-edad integer;
-YEARS INTEGER;
-BEGIN
---AQUI VA LA LOGICA DEL PROGRAMA
-YEARS:=21;
-edad:=YEARS*365;
+--creearemos una tabla simple para probar pl sql 
+CREATE TABLE DANCE(ID_DANCER integer, NOMBRE varchar2(40), EDAD integer);
+--TENEMOS EL SERVIDOR EN MODO DE DESARROLLADOR.
+--VAMOS A EMPEZAR CON UN BLOQUE PL SQL
+SET SERVEROUTPUT ON;
 
-DBMS_OUTPUT.PUT_LINE('TU EDAD EN DIA ES'|| edad);
+-- SE DECLARA LAS VARIABLES
+ DECLARE 
+EDAD INTEGER;
+YEARS INTEGER;
+
+--AQUI VA LA LOGICA DEL PROGRAMA
+BEGIN
+YEARS:=21;
+EDAD:=YEARS*365;
+--IMPRIME EL RESULTADO
+DBMS_OUTPUT.PUT_LINE('TU EDAD EN DIAS ES: '||EDAD);
 END;
 /
---crear un bloque SQL que genere 200,0000registros en secuencia ascendiente en la table dance
+
+-- CREAR UN BLOQUE PL SQL QUE GENERE 200,000 REGIISTRO EN SECUENCIA ASCENDENTE EN LA TABLA DANCE
 BEGIN
 
 FOR I IN 1..200000 LOOP
-INSERT INTO DANCE VALUES(I,'julio',21);
+INSERT INTO DANCE VALUES(I,'CAMPOS',44);
+     
 END LOOP;
 END;
 /
---HACER UN SELECT 
+  --HACEMOS UN SELECT PARA VER QUE DIABLOS HIZO
 SELECT * FROM DANCE;
 
---sistema de peliculas la de sala sera de tarea
-CREATE TABLE PELICULA(ID_PELICULA INTEGER,
-TITULO VARCHAR2(120),
-SIPNOSIS VARCHAR2(500),
-CLASIFICACION VARCHAR2(5),
-CONSTRAINT PK_ID_PELICULA PRIMARY KEY(ID_PELICULA));
-DESCRIBE PELICULA;
+--SISTEMITA DE PELICULAS LA DE SALA SERA TU TAREA
 
-CREATE TABLE HORARIO (ID_HORARIO INTEGER,
-ID_PELICULA INTEGER,
-HORARIO VARCHAR2(8),
-CONSTRAINT PK_ID_HORARIO PRIMARY KEY(ID_HORARIO),
-CONSTRAINT FK1_ID_PELICULA FOREIGN KEY(ID_PELICULA) REFERENCES PELICULA(ID_PELICULA));
+CREATE TABLE PELICULA (ID_PELICULA INTEGER,
+                      TITULO VARCHAR2(120),
+                      SINOPSIS VARCHAR2(500),
+                      CALSIFICACION VARCHAR2(5),
+                      CONSTRAINT PK_ID_PELICULA PRIMARY KEY( ID_PELICULA)
+                      );
+                      
+                     DESCRIBE  PELICULA;
+CREATE TABLE HORARIOS (ID_HORARIO INTEGER,
+                       ID_PELICULA INTEGER,
+                       HORARIO VARCHAR2(8),
+                       CONSTRAINT PK_ID_HORARIO PRIMARY KEY(ID_HORARIO),
+                       CONSTRAINT FK1_ID_PELICULA FOREIGN KEY(ID_PELICULA) REFERENCES PELICULA(ID_PELICULA)
+                       );
+        
+        DESCRIBE HORARIOS;
+        
+        --una secuencia en oracle sirve par enlazar una tabla a un pk auto_increment
+        CREATE SEQUENCE SEC_PELICULA;
+        STAR WITH 1
+        INCREMENT BY 1
+        NOMAXVALUE;
+        
+        --NOTA solo se puede asociar una sola tabla
+        --para asociar se necesita un nuevo objeto de base de datos este objeto va a serr el q insertara el o los nuevos registros
+        --en la tabla. JAMAS se usa un INSERT INTO en oracle cuando tiene un (PRIMARY KEY auto-generado) 
+        --TAREA****** INJECCION DE SQL Y CASOS REALES NOTABLES
+        
+        SET SERVEROUTPUT ON;
+        CREATE OR REPLACE PROCEDURE HOLA_MUNDO( NOMBRE IN VARCHAR2)
+        AS
+        --LOGICA
+        BEGIN 
+        DBMS_OUTPUT.PUT_LINE('HOLA COMO ESTAS:  '||NOMBRE);
+        END;
+        /
+        
+        DECLARE
+        VALOR NUMBER;
+        BEGIN
+        SUMA(12,8,VALOR);
+        SYS.dbms_output.put_line('LA SUMA ES: '||VALOR);
+        
+        HOLA_MUNDO ('JULIO');
+        END;
+        /
+        
+        
+
